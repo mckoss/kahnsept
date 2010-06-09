9437db26ee9c7a6a6edacbc8ef4ff79a5b8d82d7
Mike Koss <mckoss@startpad.org>
2010-06-09 15:17:10 -0700

    First failing test.

---
+++ kahnsept.js
@@ -0,0 +1,15 @@
+namespace.lookup('com.pageforest.kahnsept').defineOnce(function (ns) {
+
+    function Schema(name) {
+        this.name = name;
+        this.props = [];
+    }
+
+    Schema.methods({
+        addProp: function (name, type) {
+        },
+
+        delProp: function(name) {
+        }
+    });
+});
