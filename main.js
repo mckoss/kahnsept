8fb9feb6c16025d227a85040cb0fdefc14fd6b46
Chris Koss <chris@mckoss.com>
2010-06-23 15:53:27 -0700

    added Kahnsept Display file

---
+++ main.js
@@ -0,0 +1,248 @@
+namespace.lookup('com.pageforest.examples.kahnsept').defineOnce(function (ns) {
+    var clientLib = namespace.lookup('com.pageforest.client');
+    var base = namespace.lookup('org.startpad.base');
+    var schemas = new Array();
+    var schemaTitles = new Array();
+    
+    
+    // Initialize the document - create a client helper object
+    function onReady() {
+        $('#title').focus();
+        ns.client = new clientLib.Client(ns);
+        // Quick call to poll - don't wait a whole second to try loading
+        // the doc and logging in the user.
+        ns.client.poll();
+    }
+
+    // This function is called whenever your document should be reloaded.
+    function setDoc(json) {
+        $('#title').val(json.title);
+        if(json.blob.schemas)
+        	schemas = json.blob.schemas;
+        displaySchema();
+    }
+
+    // Convert your current state to JSON with title and blob properties,
+    // these will then be saved to pageforest's storage.
+    function getDoc() {
+        return {
+            'title': $('#title').val(),
+            'blob': {
+    		'schemas': schemas,
+    	},
+        };
+    }
+
+    // Called on any api errors.
+    function onError(status, message) {
+        $('#error').text(message);
+    }
+
+    // Called when the current user changes (signs in or out)
+    function onUserChange(username) {
+        var isSignedIn = username != undefined;
+        $('#username').text(isSignedIn ? username : 'anonymous');
+        $('#signin').val(isSignedIn ? 'Sign Out' : 'Sign In');
+    }
+
+    // Sign in (or out) depending on current user state.
+    function signInOut() {
+        var isSignedIn = ns.client.username != undefined;
+        if (isSignedIn) {
+            ns.client.signOut();
+        }
+        else {
+            ns.client.signIn();
+        }
+    }
+
+    function onStateChange(newState, oldState) {
+        $('#doc-state').text(newState);
+        $('#error').text('');
+
+        // Allow save if doc is dirty OR not bound (yet) to a document.
+        if (ns.client.isSaved()) {
+            $('#save').attr('disabled', 'disabled');
+        }
+        else {
+            $('#save').removeAttr('disabled');
+        }
+
+        // Refresh links on the page, too
+        var url = ns.client.getDocURL();
+        var link = $('#document');
+        if (url) {
+            link.attr('href', url + '?callback=document').show();
+        }
+        else {
+            link.hide();
+        }
+        $('#mydocs').attr('href', 'http://' + ns.client.wwwHost + '/docs/');
+        $('#app-details').attr('href', 'http://' + ns.client.wwwHost +
+                               '/apps/' + ns.client.appid);
+    }
+    
+    function displaySchema() {
+    	$('#schemas').empty();
+    	schemaTitles = base.map(schemas, function(v){return v.title});
+    	
+    	for(i = 0; i < schemas.length; i++)
+    		{	
+    		if(schemas[i].expanded == true){
+    		$('#schemas').append('<div><input id="txtS'+i+'" type="text" value="'+schemas[i].title+'" style="width:300px" /><input type="button" value="Save" onclick="kahnsept.colSchema('+i+');" /><input type="button" value="Delete" onclick="kahnsept.delSchema('+i+')" /><ul>');
+    		
+	    		
+	    		
+	    		for(j = 0; j < schemas[i].props.length; j++)
+	    		{
+	    			$('#schemas').append('<li><div><input type="text" value="' + schemas[i].props[j].propname +
+	    					'" style="width:300px" id="props' + i + 'p' + j + '" /><input type="text" value="' + schemas[i].props[j].accepted +
+	    					'" style="width:300px" id="acpts' + i + 'p' + j + '"/><input type="button" value="Del" onclick="kahnsept.delProp('+i+" , "+j+')"></div></li>');
+	    			var autotemp = $('#acpts' + i + 'p' + j);
+		    		autotemp.autocomplete({source : schemaTitles});
+	    		}
+	    		
+	    			
+	    			
+    			
+    			 $('#schemas').append('<li>Add Property<input type="button" value="Add" onclick="kahnsept.addProp('+ i + ');" /></li></ul></div>');
+	    		
+	    		
+    		} 
+    		else {
+    			$('#schemas').append('<div>'+schemas[i].title+'<input type="button" value="Edit" onclick="kahnsept.expSchema('+i+');" /></div>');
+    			}
+    		}
+    	
+    	displayInst();
+    }
+    
+    function addSchema() {
+    	saveText();
+    	schemas.push({ "title": "NewSchema", "props": new Array({"propname" : "Title" , "accepted" : "Text"}), "instances": new Array(), "expanded": true, "instexp": true});
+		displaySchema();
+    }
+    
+    function delSchema(schemanum) {
+    	saveText();
+    	schemas.splice(schemanum, 1);
+    	displaySchema();
+    }
+    
+    function addProp(schemanum){
+    	saveText();
+    	schemas[schemanum].props.push({"propname" : "NewPropName" , "accepted" : "Accepted Schema"});
+    	displaySchema();
+    }
+    
+    function delProp(schemanum, propnum){
+    	saveText();
+    	schemas[schemanum].props.splice(propnum, 1);
+    	displaySchema();
+    }
+    
+    function displayInst(){
+    	$('#instances').empty();
+    	pile = "";
+    	for(i=0;i<schemas.length;i++){
+    		if(schemas[i].instexp == true){
+	    		pile += ('<li>'+schemas[i].title+' ('+schemas[i].instances.length+') <input type="button" value="Save" onclick="kahnsept.colInst('+i+');" /><ul>');
+	    		for(j=0;j<schemas[i].instances.length;j++){
+	    			pile+=('<li>Property : Accepted Schemas <input type="button" value="Delete" onclick="kahnsept.delInst('+i+','+j+');" /><ul>');
+	    			for(k=0;k<schemas[i].props.length;k++){
+	    				temp = schemas[i].props[k].accepted;
+	    				if (schemas[i].instances[j][schemas[i].props[k].propname])
+	    					temp = schemas[i].instances[j][schemas[i].props[k].propname];
+    						    				
+	    				pile+=('<li>'+schemas[i].props[k].propname+'<input type="text" value="'+temp+'" id="s'+i+'i'+j+'p'+k+'" style="width:300px" /></li>');
+	    			}
+	    			pile+= ('</ul></li>');
+	    		}
+	    		pile+=('<li><input type="button" value="Add Instance" onclick="kahnsept.addInst('+i+')" /></li></ul></li>');
+    		}
+    		else {
+    			pile += ('<li>'+schemas[i].title+' ('+schemas[i].instances.length+') <input type="button" value="Edit" onclick="kahnsept.expInst('+i+');" /></li>');
+    		}
+    	}
+    	$('#instances').append(pile);
+    }
+    
+    function addInst(schemanum){
+    	saveText();
+    	schemas[schemanum].instances.push({"title": ('instance of '+schemas[schemanum].title+'')});
+    	displayInst();
+    }
+    
+    function delInst(schemanum, instnum){
+    	saveText();
+    	schemas[schemanum].instances.splice(instnum, 1);
+    	displayInst();
+    }
+    
+    function expSchema(schemanum){
+    	saveText();
+    	schemas[schemanum].expanded = true;
+    	displaySchema();
+    }
+    
+    function colSchema(schemanum){
+    	saveText();
+    	schemas[schemanum].expanded = false;
+    	displaySchema();
+    }
+    
+    function expInst(schemanum){
+    	schemas[schemanum].instexp = true;
+    	displaySchema();
+    }
+    
+    function colInst(schemanum){
+    	saveText();
+    	schemas[schemanum].instexp = false;
+    	displaySchema();
+    }
+    
+    function saveText() {
+    	for(i=0;i<schemas.length;i++){
+    				   		
+	    	   		for(j=0;j<schemas[i].props.length;j++){
+	    	   			if(schemas[i].expanded == true){
+	    	   				schemas[i].props[j].propname = $('#props'+i+'p'+j+'').val();
+	    	   				schemas[i].props[j].accepted = $('#acpts'+i+'p'+j+'').val();
+	    	   			}
+	    	   			
+		    	   		for(k=0;k<schemas[i].instances.length;k++){
+		    	   			schemas[i].instances[k][schemas[i].props[j].propname] = $('#s'+i+'i'+k+'p'+j+'').val();
+		    	   			
+		    	   			//Need to add code to read and save all of the instance values
+		    	   		}
+	    	   		}
+	    	   		if(schemas[i].expanded)
+	    	   			schemas[i].title = $('#txtS'+i).val();
+    			
+    	}
+    	getDoc();
+    }
+
+    // Exported functions
+    ns.extend({
+        onReady: onReady,
+        getDoc: getDoc,
+        setDoc: setDoc,
+        onError: onError,
+        onUserChange: onUserChange,
+        onStateChange: onStateChange,
+        signInOut: signInOut,
+        addSchema: addSchema,
+        delSchema: delSchema,
+        addProp: addProp,
+        delProp: delProp,
+        addInst: addInst,
+        delInst: delInst,
+        expSchema: expSchema,
+        colSchema: colSchema,
+        expInst: expInst,
+        colInst: colInst
+    });
+
+});
---
+++ test-kahnsept.js
@@ -0,0 +1,356 @@
+namespace.lookup('com.pageforest.kahnsept.test').defineOnce(function (ns) {
+    var kahnsept = namespace.lookup('com.pageforest.kahnsept');
+    var base = namespace.lookup('org.startpad.base');
+
+    var kahnseptClasses = ['Schema', 'Property', 'BuiltIn', 'Instance',
+                           'Relationship', 'World'
+                          ];
+
+    var schemaMethods = ['addProp', 'delProp'];
+
+    function addTests(ts) {
+        ts.addTest("Exported Classes", function (ut) {
+            // All Kahnsept Classes exist
+            for (var i = 0; i < kahnseptClasses.length; i++) {
+                var symbol = kahnseptClasses[i];
+                ut.assertEq(typeof kahnsept[symbol], 'function', symbol);
+            }
+        });
+
+        ts.addTest("World", function(ut) {
+            // Worlds can be created
+            var w = new kahnsept.World();
+            ut.assert(w != undefined);
+
+            // New schemas are assigned to most recent world
+            var s = new kahnsept.Schema('test');
+            ut.assertIdent(s.world, w);
+        });
+
+        ts.addTest("Schema", function(ut) {
+            var w = new kahnsept.World();
+            var s = new kahnsept.Schema('test');
+            // Name of Schema can be assigned
+            ut.assertEq(s.name, 'test');
+            // Schemas have no props when created
+            ut.assertEq(base.keys(s.props).length, 0);
+
+            // Expect Errors - Test - Properties cannot be added without
+            // type
+            var fThrows = false;
+            try {
+                s.addProp('prop1', 'Bogus');
+            } catch (e) {
+                fThrows = true;
+                ut.assertException(e, "Invalid schema");
+            }
+            ut.assert(fThrows);
+
+            s.addProp('prop1', 'String');
+            // Property successfuly added
+            ut.assertEq(base.keys(s.props).length, 1);
+            // Prop is a Property
+            ut.assert(s.props['prop1'] instanceof kahnsept.Property);
+            // Propname correctly assigned
+            ut.assertEq(s.props['prop1'].name, 'prop1');
+
+            // Props can be deleted
+            s.delProp('prop1');
+            ut.assertEq(base.keys(s.props).length, 0);
+        });
+
+        ts.addTest("Instances", function(ut) {
+            var w = new kahnsept.World();
+            var s = new kahnsept.Schema('test');
+            s.addProp("s1", "String");
+            s.addProp("n1", "Number");
+            s.addProp("b1", "Boolean");
+            s.addProp("d1", "Date");
+
+            var obj = s.createInstance();
+            ut.assertEq(typeof obj, 'object');
+            ut.assert(obj instanceof kahnsept.Instance);
+
+            obj.setProp("s1", "hello");
+            ut.assertEq(typeof obj.s1, 'string');
+            ut.assertEq(obj.s1, "hello");
+
+            obj.setProp("n1", 7);
+            ut.assertEq(typeof obj.n1, 'number');
+            ut.assertEq(obj.n1, 7);
+
+            obj.setProp("b1", true);
+            ut.assertEq(typeof obj.b1, 'boolean');
+            ut.assertEq(obj.b1, true);
+
+            var d = new Date();
+            obj.setProp("d1", d);
+            ut.assertEq(typeof obj.d1, 'object');
+            ut.assertIdent(obj.d1.constructor, Date);
+            ut.assertEq(obj.d1.getTime(), d.getTime());
+
+            var fThrows = false;
+            try {
+                obj.setProp("missing", 1);
+            } catch (e) {
+                fThrows = true;
+                ut.assertException(e, "does not exist");
+            }
+            ut.assert(fThrows);
+        });
+
+        ts.addTest("Property type conversion", function(ut) {
+            var w = new kahnsept.World();
+            var s = new kahnsept.Schema('test');
+            s.addProp("s1", "String");
+            s.addProp("n1", "Number");
+            s.addProp("b1", "Boolean");
+            s.addProp("d1", "Date");
+
+            var obj = s.createInstance();
+
+            obj.setProp("s1", 1);
+            ut.assertEq(typeof obj.s1, 'string');
+            ut.assertEq(obj.s1, "1");
+
+            obj.setProp("n1", "1.23");
+            ut.assertEq(typeof obj.n1, 'number');
+            ut.assertEq(obj.n1, 1.23);
+
+            obj.setProp("b1", "true");
+            ut.assertEq(typeof obj.b1, 'boolean');
+            ut.assertEq(obj.b1, true);
+
+            obj.setProp("d1", "1/1/2010");
+            ut.assertEq(typeof obj.d1, 'object');
+            ut.assertEq(obj.d1.constructor, Date);
+            ut.assertEq(obj.d1.getTime(), (new Date("1/1/2010")).getTime());
+        });
+
+        ts.addTest("Multi-valued properties", function(ut) {
+            var w = new kahnsept.World();
+            var s = new kahnsept.Schema('test');
+            s.addProp("s1", "String", undefined, 'many');
+            s.addProp("s2", "String", undefined, 'many');
+
+            var obj = s.createInstance({'s2': ['hello', 'mom']});
+
+            ut.assertEq(obj.s2, ['hello', 'mom']);
+
+            obj.setProp("s1", 'test');
+            ut.assertEq(obj.s1, ['test']);
+
+            obj.setProp("s1", 'test2');
+            ut.assertEq(obj.s1, ['test', 'test2']);
+        });
+
+        ts.addTest("Complex Schema", function(ut) {
+            var w = new kahnsept.World();
+            var coord = new kahnsept.Schema('coordinate');
+            coord.addProp("x", "Number");
+            coord.addProp("y", "Number");
+
+            var t = new kahnsept.Schema('test');
+            t.addProp("p1", "coordinate");
+
+            var t1 = t.createInstance();
+            t1.setProp('p1', {'x': 10, 'y': 20});
+            ut.assertEq(typeof t1.p1, 'object');
+            ut.assertEq(t1.p1._schema.name, 'coordinate');
+            ut.assertEq(typeof t1.p1.x, 'number');
+            ut.assertEq(t1.p1.x, 10);
+            ut.assertEq(typeof t1.p1.y, 'number');
+            ut.assertEq(t1.p1.y, 20);
+        });
+
+        ts.addTest("Default properties", function(ut) {
+            var w = new kahnsept.World();
+            var coord = new kahnsept.Schema('Coordinate');
+            coord.addProp("x", "Number");
+            coord.addProp("y", "Number");
+
+            var s = new kahnsept.Schema('test');
+            s.addProp("s1", "String", "default");
+            s.addProp("n1", "Number", 123);
+            s.addProp("b1", "Boolean", false);
+            s.addProp("d1", "Date", new Date("1/1/2010"));
+            s.addProp("c1", "Coordinate", {'x': 10, 'y': 20});
+
+            var obj = s.createInstance();
+
+            ut.assertEq(typeof obj.s1, 'string');
+            ut.assertEq(obj.s1, "default");
+
+            ut.assertEq(typeof obj.n1, 'number');
+            ut.assertEq(obj.n1, 123);
+
+            ut.assertEq(typeof obj.b1, 'boolean');
+            ut.assertEq(obj.b1, false);
+
+            ut.assertEq(typeof obj.d1, 'object');
+            ut.assertEq(obj.d1.constructor, Date);
+            ut.assertEq(obj.d1.getTime(), (new Date("1/1/2010")).getTime());
+
+            ut.assertEq(typeof obj.c1, 'object');
+            ut.assertEq(obj.c1._schema.name, 'Coordinate');
+            ut.assertEq(obj.c1.x, 10);
+            ut.assertEq(obj.c1.y, 20);
+        });
+
+        ts.addTest("Kahnsept Video Demo", function(ut) {
+            var w = new kahnsept.World();
+            var person = new kahnsept.Schema('Person');
+            person.addProp('name');
+            person.addProp('age', 'Number');
+
+            var deb = person.createInstance();
+            deb.setProp('name', "Debbie");
+            deb.setProp('age', 29);
+
+            var mike = person.createInstance();
+            mike.setProp('name', "Mike");
+            mike.setProp('age', 49);
+
+            ut.assertEq(mike.name, "Mike");
+            ut.assert(mike.age > deb.age);
+
+            new kahnsept.Relationship('Person', 'Person',
+                                      {'names': ['husband', 'wife']});
+
+            mike.setProp('wife', deb);
+            ut.assertIdent(mike.wife, deb);
+            ut.assertIdent(deb.husband, mike);
+
+            var fred = person.createInstance();
+            fred.setProp('name', "Fred");
+            fred.setProp('age', 21);
+
+            fred.setProp('wife', deb);
+            ut.assertIdent(fred.wife, deb);
+            ut.assertIdent(deb.husband, fred);
+            ut.assertIdent(mike.wife, undefined);
+
+            var s = JSON.stringify(w.toJSON(), undefined, 4);
+            ut.assertEq(typeof s, 'string');
+        });
+
+        ts.addTest("delProp", function(ut) {
+            var w = new kahnsept.World();
+            var person = new kahnsept.Schema('person');
+            var address = new kahnsept.Schema('address');
+
+            person.addProp('residence', 'address', undefined, 'one');
+
+            ut.assert(person.props['residence'] != undefined);
+            ut.assert(address.props['person'] != undefined);
+
+            person.delProp('residence');
+
+            ut.assertEq(person.props['residence'], undefined);
+            ut.assertEq(address.props['person'], undefined);
+        });
+
+        ts.addTest("One to Many", function(ut) {
+            var w = new kahnsept.World();
+            var parent = new kahnsept.Schema('Parent');
+            var child = new kahnsept.Schema('Child');
+
+            new kahnsept.Relationship('Child', 'Parent',
+                                      {'cards': ['one', 'many']});
+
+            var p1 = parent.createInstance();
+            var p2 = parent.createInstance();
+            var c1 = child.createInstance();
+            var c2 = child.createInstance();
+
+            //Single Connection
+            p1.setProp('child', c1);
+            ut.assertIdent(p1.child[0], c1);
+            ut.assertIdent(c1.parent, p1);
+
+            //One parent, two children
+            p1.setProp('child', c2);
+            ut.assertEq(p1.child.length, 2);
+            ut.assertIdent(p1.child[1], c2);
+            //ut.assert(p1.hasValue("child", c1));
+            //ut.assert(p1.hasValue("child", c2));
+            ut.assertIdent(c2.parent, p1);
+
+            //"Steal" child into second parent
+            p2.setProp('child', c2);
+            ut.assertEq(p1.child.length, 1);
+            ut.assertEq(p2.child.length, 1);
+            ut.assertIdent(p1.child[0], c1);
+            ut.assertIdent(p2.child[0], c2);
+        });
+
+        ts.addTest("Many to Many", function(ut) {
+            var w = new kahnsept.World();
+            var model = new kahnsept.Schema('Model');
+            var color = new kahnsept.Schema('Color');
+
+            new kahnsept.Relationship('Color', 'Model',
+                                      {'cards': ['many', 'many']});
+
+            var m1 = model.createInstance();
+            var m2 = model.createInstance();
+            var c1 = color.createInstance();
+            var c2 = color.createInstance();
+
+            m1.setProp('color', c1);
+            m1.setProp('color', c2);
+            ut.assertEq(m1.color.length, 2);
+            ut.assertEq(c1.model.length, 1);
+            ut.assertEq(c2.model.length, 1);
+
+            m2.setProp('color', c1);
+            m2.setProp('color', c2);
+            ut.assertEq(m1.color.length, 2);
+            ut.assertEq(m2.color.length, 2);
+            ut.assertEq(c1.model.length, 2);
+            ut.assertEq(c2.model.length, 2);
+        });
+
+        ts.addTest("JSON", function(ut) {
+            var w = new kahnsept.World();
+            var person = new kahnsept.Schema('Person');
+            person.addProp('name');
+            person.addProp('age', 'Number');
+
+            new kahnsept.Relationship('Person', 'Person',
+                                      {'names': ['husband', 'wife']});
+
+            new kahnsept.Relationship('Person', 'Person',
+                                      {'names': ['parents', 'children'],
+                                       'cards': ['many', 'many']});
+
+            var deb = person.createInstance({name: "Debbie",
+                                             age: 29});
+            var mike = person.createInstance({name: "Mike",
+                                              age: 49,
+                                              wife: deb});
+            var chris = person.createInstance({name: "Chris",
+                                               age: 21,
+                                               parents: [mike, deb]});
+            var fred = person.createInstance({name: "Fred",
+                                              age: 21,
+                                              wife: deb});
+
+            var s = JSON.stringify(w.toJSON(), undefined, 4);
+            ut.assertEq(typeof s, 'string');
+            console.log(s);
+
+            var json = JSON.parse(s);
+            var s2 = JSON.stringify(json, undefined, 4);
+            ut.assertEq(s, s2);
+
+            var w2 = new kahnsept.World();
+            w2.importJSON(json);
+            var s3 = JSON.stringify(w2.toJSON(), undefined, 4);
+            ut.assertEq(s, s3);
+            console.log(s3);
+        });
+    }
+
+    ns.addTests = addTests;
+});
